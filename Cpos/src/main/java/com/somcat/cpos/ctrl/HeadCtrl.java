3c4f678b9828a3a80d245c26d65c5d6777dfd961
jo066 <airbag0012@gmail.com>
2020-07-23 17:50:33 +0900

    soldlist folder modified n headCtrl add function

--- Cpos/src/main/java/com/somcat/cpos/ctrl/HeadCtrl.java
+++ Cpos/src/main/java/com/somcat/cpos/ctrl/HeadCtrl.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.somcat.cpos.service.HeadServiceIntf;
@@ -17,5 +18,10 @@ public class HeadCtrl {
 	
 	@Inject
 	HeadServiceIntf hsv;	
+	
+	@GetMapping("/plist")
+	public void plist() {
+		
+	}
 	
 }
